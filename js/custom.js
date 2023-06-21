0cff4f851c06e248cb635784f18c2036e0a1c108
anildalar <dollorinfotech@gmail.com>
2023-06-21 09:05:30 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+let x = ['Anil'];
+
+let y = [...x,'DOLLOR'];
+
+console.log(y);
+
+let n1=['Twitter']
+
+let news= [...n1,'boss '];
+let news2 = [...news,'Elon Musk'];
+
+console.log(news2);
---
+++ js/custom2.js
@@ -0,0 +1,10 @@
+
+
+//1. function defination
+function getMyFullName(...args){
+    console.log(args);
+
+}
+
+//2. function calling/invoking
+getMyFullName('Anil',"Dollor",30,5.6,["Khushansh","Khushi"]);
